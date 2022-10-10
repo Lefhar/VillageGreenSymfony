233fc20167562f39a30a75219781f8b9ea52c35a
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-10-10 20:19:42 +0200

    sécurisation des factures

--- village_green/src/Controller/FactureController.php
+++ village_green/src/Controller/FactureController.php
@@ -6,6 +6,7 @@ use App\Entity\Orders;
 use App\Repository\CustomersRepository;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Knp\Snappy\Pdf;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +14,13 @@ use Symfony\Component\HttpFoundation\Response;
 class FactureController extends AbstractController
 {
     /**
+     * @IsGranted("ROLE_USER")
      * @Route("/facture/pdf/{order}", name="facturePdf")
      */
 
-    public function facturePdf(Pdf $knpSnappyPdf, Orders $order, CustomersRepository $cust)
+    public function facturePdf(Pdf $knpSnappyPdf, Orders $order)
     {
-        if ($order->getCustomer() !== $cust->findOneBy(['users' => $this->getUser()]) and $this->getUser()) {
+        if ($order->getCustomer()->getUsers() !== $this->getUser()) {
             return $this->redirectToRoute('accueil');
         }
         $html = $this->renderView('facture/index.html.twig', array(
@@ -29,8 +31,25 @@ class FactureController extends AbstractController
             'facture_numero_'. $order->getId().'.pdf'
         );
     }
+    /**
+     * @IsGranted("ROLE_ADMIN")
+     * @Route("/facture/pdfadmin/{order}", name="facturePdfadmin")
+     */
+
+    public function factureAdminPdf(Pdf $knpSnappyPdf, Orders $order)
+    {
+
+        $html = $this->renderView('facture/index.html.twig', array(
+            'order' => $order
+        ));
+        return new PdfResponse(
+            $knpSnappyPdf->getOutputFromHtml($html),
+            'facture_numero_'. $order->getId().'.pdf'
+        );
+    }
 
     /**
+     * @isGranted("ROLE_USER")
      * @Route("/facture/vu/{order}", name="facture")
      */
 
